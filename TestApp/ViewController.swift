70f317e05687b8b21bb1c77352198bf58053e261
BuildMachine <buildmachine@Tasos-MacBook-Pro.local>
2017-05-02 15:44:35 -0400

    not compiling

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("hello")
-        return false;
+        return true;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
