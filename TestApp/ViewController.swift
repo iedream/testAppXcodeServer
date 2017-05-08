9c06919c2a712241c8fcd1ba72c3a25ff3b3bed4
Catherine Zhao <catherinez@ecobee.com>
2017-05-08 18:34:49 -0400

    test

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("hello")
+        var hi = "hello";
         // Do any additional setup after loading the view, typically from a nib.
     }
 
